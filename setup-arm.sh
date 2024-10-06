4faf7b5da56d8c66153401b1f040325152cd6ae7
Evan Shoshan <evan.shoshan@gmail.com>
2024-10-06 14:49:58 -0400

    updates for rpi

--- setup-arm.sh
+++ setup-arm.sh
@@ -1,5 +1,11 @@
 echo "setting up ARM device"
-sudo apt-get update && sudo apt-get upgrade
+sudo apt-get -yqq update && sudo apt-get -y upgrade
+
+echo "install utils"
+sudo apt-get install -y neovim bat fzf
+
+echo "setting up gh"
+sudo apt-get install -y gh
 
 echo "Installing awscli...."
 curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip"
@@ -7,13 +13,38 @@ unzip awscliv2.zip
 sudo ./aws/install
 
 echo "Install docker..."
-sudo apt-get install -y docker.io
-sudo systemctl start docker.service
-sudo systemctl enable docker.service
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -yqq docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo usermod -aG docker ${USER}
 
 echo "Installing Python..."
-sudo apt-get install -y libffi-dev libssl-dev
-sudo apt-get install -y python3-dev python3 python3-pip
+#sudo apt-get install -y libffi-dev libssl-dev
+#sudo apt-get install -y python3-dev python3 python3-pip
 
 echo "Installing docker compose..."
-sudo pip3 install docker-compose
+sudo touch /usr/bin/docker-compose
+echo 'docker compose --compatibility "$@"' | sudo tee /usr/bin/docker-compose
+sudo chmod +x /usr/bin/docker-compose
+
+echo "Installing terraform"
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
+sudo apt-get install -y terraform
